0f6264b7e7723a2d3638e031d6aa007bb7b84e07
himanshuj233 <himanshuj233@gmail.com>
2021-07-23 11:23:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import json
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+
+
+
+@app.route('/status')
+def healthcheck():
+    response = app.response_class(
+        response=json.dumps({"result":"OK-healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    app.logger.info('Status request Successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Metrics Request Succesfull')
+    return response
+
+@app.route("/")
+def hello():
+    app.logger.info('Main request Succesfull')
+    return "Hello World!"
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    app.run(host='0.0.0.0')
